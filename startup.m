703e1dd9db8f53b8bb9dd4393828644ead94b4fb
tuannt8 <tuannt8@gmail.com>
2016-04-04 13:45:40 +0200

    Compare DSC with other methods

---
+++ startup.m
@@ -0,0 +1,9 @@
+% Change default axes fonts.
+% set(0,'DefaultAxesFontName', 'arial')
+% set(0,'DefaultAxesFontSize', 12)
+% 
+% % Change default text fonts.
+% set(0,'DefaultTextFontname', 'arial')
+% set(0,'DefaultTextFontSize', 12);
+% 
+% disp('Set font arial for figure');
